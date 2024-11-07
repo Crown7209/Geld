d16ab47cdd2c6d22a1d2330ce26fac625b56f422
Crown7209 <146717132+Crown7209@users.noreply.github.com>
2024-11-07 10:10:45 +0800

    category bolson

--- frontend/src/components/ui/AddCategory.jsx
+++ frontend/src/components/ui/AddCategory.jsx
@@ -1,50 +1,24 @@
 import { useState } from "react";
-import { BluePlusBig, BluePlusIcon, CloseIcon } from "../svg";
-import { ChooseIcons } from "./ChooseIcons";
+import { BluePlusIcon, CloseIcon } from "../svg";
 import { useFormik } from "formik";
 import { DownArrow } from "../svg";
-import {
-  Anchor,
-  BaseBall,
-  BezierCurve,
-  Exam,
-  ExcellLogo,
-  Exclude,
-  Globe,
-  Home,
-  HourGlass,
-  HouseLine,
-  IdentificationBadge,
-  IdentificationCard,
-  ImageSquare,
-  IntersectSquare,
-  Ladder,
-  Leaf,
-  ListPlus,
-  Microphone,
-  NotePad,
-  NumberFive,
-  NumberSeven,
-  Orange,
-  Paper,
-  Peace,
-  Pencil,
-  Plus,
-  Question,
-  Road,
-  Vignette,
-  Watch,
-} from "../icons";
+import { Home } from "../icons";
+import { colors, icons } from "./Data";
 
 export const AddCategory = () => {
   const [errorMessage, setErrorMessage] = useState("");
   const [open, setOpen] = useState(false);
   const [selectedOption, setSelectedOption] = useState(null);
+  const [selectedColor, setSelectedColor] = useState(null);
 
   const toggling = () => setOpen(!open);
 
   const onOptionClicked = (value) => () => {
-    setSelectedOption(value);
+    setSelectedOption(value.icon);
+  };
+
+  const onColorClicked = (value) => () => {
+    setSelectedColor(value.color);
     setOpen(false);
   };
 
@@ -94,7 +68,7 @@ export const AddCategory = () => {
 
       <dialog id="add_record" className="modal">
         <div className="modal-box max-w-[494px] w-full rounded-xl p-0">
-          <div className="px-6 py-5 flex items-center justify-between border-b border-[#E2E8F0] bg-white">
+          <div className="px-6 py-5 flex items-center justify-between border-b border-[#E2E8F0] bg-white ">
             <p className="text-xl font-semibold font-roboto text-[#0F172A]">
               Add Category
             </p>
@@ -109,11 +83,12 @@ export const AddCategory = () => {
             className="p-6 flex flex-col gap-8"
           >
             <div className="flex gap-3 h-12 relative">
-              {/* <ChooseIcons /> */}
               <div
                 onClick={toggling}
                 tabIndex={0}
-                className="p-4 flex gap-1 items-center rounded-lg border border-[#D1D5DB] bg-[#F9FAFB] cursor-pointer"
+                className={`p-4 flex gap-1 items-center rounded-lg border border-[#D1D5DB] cursor-pointer ${
+                  `bg-[${selectedColor}]` || "bg-[#F9FAFB]"
+                }`}
               >
                 <div>{selectedOption || <Home />}</div>
                 <DownArrow />
@@ -123,46 +98,30 @@ export const AddCategory = () => {
                 <div className="absolute top-[99%] left-0 max-w-[312px] w-full rounded-lg border border-[#D1D5DB] bg-[#F9FAFB]">
                   <div className="p-6 flex flex-col gap-6">
                     <div className="grid grid-cols-6 grid-rows-5 gap-6">
-                      <Home />
-                      <HouseLine />
-                      <IdentificationBadge />
-                      <IdentificationCard />
-                      <Ladder />
-                      <IntersectSquare />
-                      <ImageSquare />
-                      <Plus />
-                      <Microphone />
-                      <ExcellLogo />
-                      <NotePad />
-                      <ListPlus />
-                      <Leaf />
-                      <NumberFive />
-                      <NumberSeven />
-                      <Road />
-                      <HourGlass />
-                      <Anchor />
-                      <BezierCurve />
-                      <Exclude />
-                      <Vignette />
-                      <BaseBall />
-                      <Question />
-                      <Exam />
-                      <Watch />
-                      <Globe />
-                      <Orange />
-                      <Peace />
-                      <Paper />
-                      <Pencil />
+                      {icons.map((icon, id) => {
+                        return (
+                          <button
+                            type="button"
+                            onClick={onOptionClicked(icon)}
+                            key={id}
+                          >
+                            {icon.icon}
+                          </button>
+                        );
+                      })}
                     </div>
                     <div className="w-full h-[1px] bg-black opacity-10"></div>
                     <div className="flex gap-4">
-                      <button className="w-6 h-6 rounded-full bg-[#0166FF]"></button>
-                      <button className="w-6 h-6 rounded-full bg-[#01B3FF]"></button>
-                      <button className="w-6 h-6 rounded-full bg-[#41CC00]"></button>
-                      <button className="w-6 h-6 rounded-full bg-[#F9D100]"></button>
-                      <button className="w-6 h-6 rounded-full bg-[#FF7B01]"></button>
-                      <button className="w-6 h-6 rounded-full bg-[#AE01FF]"></button>
-                      <button className="w-6 h-6 rounded-full bg-[#FF0101]"></button>
+                      {colors.map((color, id) => {
+                        return (
+                          <button
+                            type="button"
+                            onClick={onColorClicked(color)}
+                            key={id}
+                            className={`w-6 h-6 rounded-full bg-[${color.color}]`}
+                          ></button>
+                        );
+                      })}
                     </div>
                   </div>
                 </div>
